2985720b507c36575259e4d9b7508cf805077240
tanya <75728340+tanyalzvsk@users.noreply.github.com>
2025-10-26 22:29:42 +0300

    complete hash calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import path from "path";
+import fs from "fs";
+import { createHash } from "crypto";
+
+const filePath = path.join(process.cwd(), "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-  // Write your code here
+  const hash = createHash("sha256");
+  const fileStream = fs.createReadStream(filePath);
+  fileStream.pipe(hash);
+
+  hash.on("readable", () => {
+    const data = hash.read();
+    if (data) {
+      console.log(data.toString("hex"));
+    }
+  });
 };
 
 await calculateHash();
