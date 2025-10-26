ba07684177a6b5ea6fbe5eb15f1716b904b5fc27
tanya <75728340+tanyalzvsk@users.noreply.github.com>
2025-10-26 20:30:23 +0300

    complete cli env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-  // Write your code here
+  const envVars = process.env;
+  const formattedVars = Object.entries(envVars)
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+  console.log(formattedVars);
 };
 
 parseEnv();
