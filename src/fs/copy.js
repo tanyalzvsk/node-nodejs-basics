d757473722760056da108f75aa9574ec43a7b07c
tanya <75728340+tanyalzvsk@users.noreply.github.com>
2025-10-26 20:02:13 +0300

    complete fs copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises";
+import path from "path";
+
+const sourceFolderPath = path.join(process.cwd(), "files");
+const destFolderPath = path.join(process.cwd(), "files-copy");
+
 const copy = async () => {
-  // Write your code here
+  try {
+    await fs.access(sourceFolderPath);
+    await fs.access(destFolderPath);
+    console.error("FS operation failed");
+  } catch (err) {
+    await fs.cp(sourceFolderPath, destFolderPath, { recursive: true });
+  }
 };
 
 await copy();
