3ffa7a33b80e98f669983627329b66cb6bdb1fe1
tanya <75728340+tanyalzvsk@users.noreply.github.com>
2025-10-26 20:01:04 +0300

    complete fs create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import fs from "fs/promises";
+import path from "path";
+
+const content = "I am fresh and young";
+const filePath = path.join(process.cwd(), "files", "fresh.txt");
+
 const create = async () => {
-  // Write your code here
+  try {
+    await fs.access(filePath);
+    console.error("FS operation failed");
+  } catch (err) {
+    await fs.writeFile(filePath, content);
+  }
 };
 
 await create();
