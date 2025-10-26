82333348ff3e5228f76030bbb08722230b4d1282
tanya <75728340+tanyalzvsk@users.noreply.github.com>
2025-10-26 23:12:30 +0300

    complete streams transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from "stream";
+
+const reverseTransform = new Transform({
+  transform(chunk, encoding, callback) {
+    callback(null, chunk.toString().split("").reverse().join(""));
+  },
+});
+
 const transform = async () => {
-  // Write your code here
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
