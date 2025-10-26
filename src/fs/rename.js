7f521c1c4a0dfe965c3e5613c6c461ab7d1a8635
tanya <75728340+tanyalzvsk@users.noreply.github.com>
2025-10-26 20:05:52 +0300

    complete js rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises";
+import path from "path";
+
+const wrongFilePath = path.join(process.cwd(), "files", "wrongFilename.txt");
+const rightFilePath = path.join(process.cwd(), "files", "properFilename.md");
+
 const rename = async () => {
-  // Write your code here
+  try {
+    await fs.access(wrongFilePath);
+    await fs.access(rightFilePath);
+    console.error("FS operation failed");
+  } catch (err) {
+    await fs.rename(wrongFilePath, rightFilePath);
+  }
 };
 
 await rename();
