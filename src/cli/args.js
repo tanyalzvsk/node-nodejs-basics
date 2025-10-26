b54852cd0230431b3ca6524c5696fe07b1d6cec3
tanya <75728340+tanyalzvsk@users.noreply.github.com>
2025-10-26 21:24:00 +0300

    complete cli args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-  // Write your code here
+  const argVars = process.argv.slice(2);
+  const formattedVars = [];
+
+  for (let i = 0; i < argVars.length; i += 2) {
+    const key = argVars[i].slice(2);
+    const value = argVars[i + 1];
+    formattedVars.push([key, value]);
+  }
+
+  formattedVars.forEach(([key, value]) => console.log(`${key} is ${value}`));
 };
 
 parseArgs();
