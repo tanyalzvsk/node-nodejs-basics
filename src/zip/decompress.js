b46383d371e6a43dd533515ed87546fb6c7c66b5
tanya <75728340+tanyalzvsk@users.noreply.github.com>
2025-10-26 23:39:17 +0300

    complete zip decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import path from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream";
+import { createGunzip } from "zlib";
+
+const compressedFilePath = path.join(process.cwd(), "files", "archive.gz");
+const decompressedFilePath = path.join(process.cwd(), "files", "fileToCompress.txt");
+
 const decompress = async () => {
-  // Write your code here
+  const gunzip = createGunzip();
+  const source = createReadStream(compressedFilePath);
+  const destination = createWriteStream(decompressedFilePath);
+
+  pipeline(source, gunzip, destination, (err) => {
+    if (err) {
+      console.error("failed to decompress", err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await decompress();
