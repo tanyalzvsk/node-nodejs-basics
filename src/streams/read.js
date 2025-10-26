609fcb2eab567d99cc5d029a7e3254d412ce1385
tanya <75728340+tanyalzvsk@users.noreply.github.com>
2025-10-26 23:11:35 +0300

    complete streams read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import path from "path";
+import fs from "fs";
+
+const filePath = path.join(process.cwd(), "files", "fileToRead.txt");
+
 const read = async () => {
-  // Write your code here
+  const readableStream = fs.createReadStream(filePath, { encoding: "utf-8" });
+   readableStream.pipe(process.stdout);
 };
 
 await read();
