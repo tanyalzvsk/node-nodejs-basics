c905163fba8bdac196f8668d0246934f1338b0f3
tanya <75728340+tanyalzvsk@users.noreply.github.com>
2025-10-26 20:04:17 +0300

    complete fs read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import fs from "fs/promises";
+import path from "path";
+
+const filePath = path.join(process.cwd(), "files", "fileToRead.txt");
+
 const read = async () => {
-  // Write your code here
+  try {
+    await fs.access(filePath);
+   const fileContent = await fs.readFile(filePath, "utf8");
+    console.log(fileContent);
+  } catch (err) {
+    console.error("FS operation failed");
+  }
 };
 
 await read();
