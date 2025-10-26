f0b9739db971c0043115a34876ccffa9e659a189
tanya <75728340+tanyalzvsk@users.noreply.github.com>
2025-10-26 20:03:14 +0300

    complete fs delete.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from "fs/promises";
+import path from "path";
+
+const folderPath = path.join(process.cwd(), "files");
+
 const list = async () => {
-  // Write your code here
+  try {
+    await fs.access(folderPath);
+    const filesArr = await fs.readdir(folderPath);
+    console.log(filesArr);
+  } catch (err) {
+    console.error("FS operation failed");
+  }
 };
 
 await list();
