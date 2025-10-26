bff980d27171e299ad9cf4bf6f123b93fbc4d187
tanya <75728340+tanyalzvsk@users.noreply.github.com>
2025-10-26 23:37:34 +0300

    complete zip compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import path from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream";
+import { createGzip } from "zlib";
+
+const filePath = path.join(process.cwd(), "files", "fileToCompress.txt");
+const compressedFilePath = path.join(process.cwd(), "files", "archive.gz");
+
 const compress = async () => {
-  // Write your code here
+  const gzip = createGzip();
+  const source = createReadStream(filePath);
+  const destination = createWriteStream(compressedFilePath);
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error("failed to compress", err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await compress();
